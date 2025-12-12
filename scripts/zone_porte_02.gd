0712b0ff2b5817ef0d6da755f177e1c07912e4bc
MRF <74323188+Cachton@users.noreply.github.com>
2025-12-12 17:53:33 -0500

    v0.1
    -new level added
    --collisions done
    --coding needed: entry door, exit door, level puzzle

--- scripts/zone_porte_02.gd
+++ scripts/zone_porte_02.gd
@@ -5,5 +5,5 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player") and !HUD.magicien_present and !HUD.homme_present:
-		var level03 = load("res://scenes/level_03.tscn")
-		get_tree().change_scene_to_packed(level03)
+		var endscene = load("res://scenes/endscene.tscn")
+		get_tree().change_scene_to_packed(endscene)
